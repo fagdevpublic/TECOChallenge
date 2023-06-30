8fc49cf39b9445dabcc8fded874fb4484dec8af3
fagdevpublic <138188375+fagdevpublic@users.noreply.github.com>
2023-06-30 02:18:04 -0300

    Add files via upload

---
+++ WeatherChallenge/Models/CityLocation.swift
@@ -0,0 +1,13 @@
+//
+//  CityLocation.swift
+//  WeatherChallenge
+//
+//  Created by Flavio Andres Gomez on 30/06/2023.
+//
+
+import Foundation
+
+struct CityLocation {
+    let lat: String
+    let long: String
+}
